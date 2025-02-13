a78af8d222239acb91c76eecf9a63832d59191d1
Ruumaa <titankalbuesa01@gmail.com>
2025-02-13 12:47:51 +0700

    feat: update bg image

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="w-full h-screen bg-[url('/bg-desktop.svg')] bg-cover xl:bg-center lg:bg-[center_right_68%]  bg-[center_right_36.5%] bg-no-repeat relative">
+    <main className="w-full h-screen bg-[url('/bg.png')] bg-cover xl:bg-center lg:bg-[center_right_68%]  bg-[center_right_36.5%] bg-no-repeat relative">
       {/* gradient */}
       <div className="h-full bg-gradient-to-l from-white from-[1%] bg-right -right-28 to-transparent absolute inset-0 sm:hidden z-10"></div>
 
